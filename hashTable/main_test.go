bb8334ff414d3848b4548f7158e12237338f24f2
Chase Starr <chasestarr@gmail.com>
2016-12-23 09:51:38 -0500

    hash table test

---
+++ hashTable/main_test.go
@@ -0,0 +1,18 @@
+package hashTable
+
+import "testing"
+
+func TestHash(t *testing.T) {
+	h := New(20)
+	h.Insert("dog", "corgi")
+
+	if h.Retrieve("dog") != "corgi" {
+		t.Fatal("Hash Retieve error, key: 'dog' should return: 'corgi'")
+	}
+
+	h.Insert("dog", "basset hound")
+
+	if h.Retrieve("dog") != "basset hound" {
+		t.Fatal("Hash Insert overwrite error, key: 'dog' should return: 'basset hound'")
+	}
+}
